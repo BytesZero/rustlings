f08a6002972af7e5dcf87174191c3de0f4974446
Zero <1300326388@qq.com>
2024-07-04 12:42:31 +0800

    🎉 已完成 16 个

--- exercises/03_if/if1.rs
+++ exercises/03_if/if1.rs
@@ -1,9 +1,9 @@
 fn bigger(a: i32, b: i32) -> i32 {
-    // TODO: Complete this function to return the bigger number!
-    // If both numbers are equal, any of them can be returned.
-    // Do not use:
-    // - another function call
-    // - additional variables
+    if a > b {
+        a
+    } else {
+        b
+    }
 }
 
 fn main() {
